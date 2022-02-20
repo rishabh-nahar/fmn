0caa29405ca48365a86c8b531a8231365ce6c19e
Rishabh <90189129+rishabh-nahar@users.noreply.github.com>
2022-02-20 11:37:45 +0530

    Update settings.py

--- findmynotes/findmynotes/settings.py
+++ findmynotes/findmynotes/settings.py
@@ -123,5 +123,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'findmynotes2022@gmail.com'
-EMAIL_HOST_PASSWORD = 'radheradhe1217'
+EMAIL_HOST_USER = 'fmn***@gmail.com'
+EMAIL_HOST_PASSWORD = '****************'
